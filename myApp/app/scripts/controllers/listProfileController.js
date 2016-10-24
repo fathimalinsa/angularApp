17f6a348abce22597e8780072c7fc99167b0001e
fathima linsa <linsarazak@gmail.com>
2016-10-24 09:31:17 +0530

    Added more criterias to search

--- myApp/app/scripts/controllers/listProfileController.js
+++ myApp/app/scripts/controllers/listProfileController.js
@@ -32,7 +32,10 @@ angular.module('myApp')
         user.search = false;
         $scope.searchResult = false;
         if($scope.search!="") {
-          if(( user.user_name.indexOf($scope.search) >= 0 )||( user.first_name.indexOf($scope.search) >= 0 )||( user.second_name.indexOf($scope.search) >= 0 )) {
+          if(( user.user_name.indexOf($scope.search) >= 0 )||( user.first_name.indexOf($scope.search) >= 0 )||( user.second_name.indexOf($scope.search) >= 0 )||( user.mobile.indexOf($scope.search) >= 0 )
+          ||( user.voters_id.indexOf($scope.search) >= 0 )||( user.sponser_number.indexOf($scope.search) >= 0 )||( user.sponsers_name.indexOf($scope.search) >= 0 )||( user.blood_group.indexOf($scope.search) >= 0 )
+          ||( user.height.indexOf($scope.search) >= 0 )||( user.weight.indexOf($scope.search) >= 0 )||( user.addr_line1.indexOf($scope.search) >= 0 )
+          ||( user.addr_line2.indexOf($scope.search) >= 0 )||( user.current_location.indexOf($scope.search) >= 0 )) {
             user.search = true;
             $scope.searchResult = true;
           }
